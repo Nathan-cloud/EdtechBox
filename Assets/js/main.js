826973ed9d3f0dd58a474a33de4362b26ee2fb77
Nathan-cloud <nathanmorgan867@gmail.com>
2020-10-19 00:58:40 -0700

    chnge

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -50,12 +50,43 @@ function main() {
 			}
 		});
 
-		// Nivo Lightbox
-		$('.portfolio-item a').nivoLightbox({
-			effect: 'slideDown',
-			keyboardNav: true,
-		});
-	})();
+    // Portfolio isotope filter
+    $(window).load(function() {
+      var $container = $('.portfolio-items');
+      $container.isotope({
+          filter: '*',
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+      $('.cat a').click(function() {
+          $('.cat .active').removeClass('active');
+          $(this).addClass('active');
+          var selector = $(this).attr('data-filter');
+          $container.isotope({
+              filter: selector,
+              animationOptions: {
+                  duration: 750,
+                  easing: 'linear',
+                  queue: false
+              }
+          });
+          return false;
+      });
+
+  });
+  
+
+  // Nivo Lightbox 
+  $('.portfolio-item a').nivoLightbox({
+          effect: 'slideDown',  
+          keyboardNav: true,                            
+      });
+
+  })();
+  
 
 	$(
 		'#inline-popups,#inline-popups-2,#inline-popups-3,#inline-popups-4,#inline-popups-5,#inline-popups-6'
